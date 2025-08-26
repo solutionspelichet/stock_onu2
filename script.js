98607a559dbef7163bbb8b2022268817d5683b5f
solutionspelichet <solutions.pelichet@gmail.com>
2025-08-26 15:35:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /***********************
  *  Config API
  ***********************/
-const API_BASE_URL = "https://script.google.com/macros/s/REPLACE_WITH_YOUR_DEPLOYMENT_ID/exec"; // <<< remplace ici
+const API_BASE_URL = "https://script.google.com/macros/s/AKfycbwO0P3Yo5kw9PPriJPXzUMipBrzlGTR_r-Ff6OyEUnsNu-I9q-rESbBq7l2m6KLA3RJ/exec"; // <<< remplace ici
 
 /***********************
  *  Helpers
